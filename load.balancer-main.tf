d84d38fdf10427c2a8159398b1cda97fb3ea39cb
SandeepRoshan <115892000+SandeepRoshan@users.noreply.github.com>
2024-08-26 13:16:31 +0530

    updated

--- load.balancer-main.tf
+++ load.balancer-main.tf
@@ -1,9 +1,10 @@
+
 /*
  | --
  | -- Listeners are the front-end (ears) whilst target groups are the
  | -- back-end of our load balancer setup.
  | --
- | -- This highly reusable module allows one to configure any number of
+ | -- This highly reusable module allows one t configure any number of
  | -- listeners and targets each with words like "etcd" and "rabbitmq".
  | --
  | -- At its architectural heart the application load balancer is designed
@@ -12,86 +13,26 @@
  | -- for application load balancers.
  | --
 */
-
-resource "aws_security_group" "default" {
-    name        = "default-security-group"
-    description = "Default security group"
-    vpc_id      = aws_vpc.main.id  # Ensure this is correctly set
-
-    ingress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-        Name = "default-security-group"
-    }
-}
-
-
-
-
-# Create VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "main-vpc"
-  }
-}
-
-# Create Subnet
-resource "aws_subnet" "example" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "example-subnet"
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-resource "aws_alb" "alb" {
-    name               = "${var.in_ecosystem_name}-${var.in_tag_timestamp}"
-    security_groups    = [aws_security_group.default.id]
-    subnets            = [aws_subnet.example.id]
-    internal           = var.in_is_internal
-    load_balancer_type = var.in_lb_class
+resource aws_alb alb {
+    name               = "${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
+    security_groups    = [ "${var.in_security_group_ids}" ]
+    subnets            = [ "${var.in_subnet_ids}" ]
+    internal           = "${ var.in_is_internal ? "true" : "false" }"
+    load_balancer_type = "${var.in_lb_class}"
 
     enable_deletion_protection = false
     idle_timeout               = 60
     ip_address_type            = "ipv4"
 
     tags = {
-        Name     = "${var.in_lb_class}-load-balancer-${var.in_ecosystem_name}-${var.in_tag_timestamp}"
-        Class    = var.in_ecosystem_name
-        Instance = "${var.in_ecosystem_name}-${var.in_tag_timestamp}"
-        Desc     = "This ${var.in_is_internal ? "in" : "ex"}ternal ${var.in_lb_class} load balancer for ${var.in_ecosystem_name} ${var.in_tag_description}"
+        Name = "${var.in_lb_class}-load-balancer-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
+        Class = "${ var.in_ecosystem_name }"
+        Instance = "${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
+        Desc   = "This ${ var.in_is_internal ? "in" : "ex" }ternal ${var.in_lb_class} load balancer for ${ var.in_ecosystem_name } ${ var.in_tag_description }"
     }
+
 }
+
 
 /*
  | --
@@ -107,19 +48,19 @@ resource "aws_alb" "alb" {
  | -- layer 4 (in contrast to NLB operating at layer 7).
  | --
 */
+resource aws_alb_listener http_listener {
+    count = "${ length( var.in_front_end ) }"
 
-resource "aws_alb_listener" "http_listener" {
-    count = length(var.in_front_end)
-
-    load_balancer_arn = aws_alb.alb.arn
-    port              = element(var.protocols[var.in_front_end[count.index]], 1)
-    protocol          = element(var.protocols[var.in_front_end[count.index]], 0)
+    load_balancer_arn = "${ aws_alb.alb.arn }"
+    port              = "${ element( var.protocols[ var.in_front_end[ count.index ] ], 1 ) }"
+    protocol          = "${ element( var.protocols[ var.in_front_end[ count.index ] ], 0 ) }"
 
     default_action {
-        target_group_arn = element(aws_alb_target_group.alb_targets.*.arn, count.index)
+        target_group_arn = "${ element( aws_alb_target_group.alb_targets.*.arn, count.index ) }"
         type = "forward"
     }
 }
+
 
 /*
  | --
@@ -134,22 +75,23 @@ resource "aws_alb_listener" "http_listener" {
  | -- the implementation on port 15672.
  | --
 */
-
-resource "aws_alb_target_group" "alb_targets" {
-    count       = length(var.in_back_end)
-    name        = "${substr(var.in_lb_class, 0, 3)}-${var.in_ecosystem_name}-${var.in_tag_timestamp}-${count.index}"
-    protocol    = element(var.protocols[var.in_back_end[count.index]], 0)
-    port        = element(var.protocols[var.in_back_end[count.index]], 1)
-    vpc_id      = aws_vpc.main.id
+resource aws_alb_target_group alb_targets{
+    count       = "${ length( var.in_back_end ) }"
+    name        = "${ substr( var.in_lb_class, 0, 3 ) }-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
+    protocol    = "${ element( var.protocols[ var.in_back_end[ count.index ] ], 0 ) }"
+    port        = "${ element( var.protocols[ var.in_back_end[ count.index ] ], 1 ) }"
+    vpc_id      = "${ var.in_vpc_id }"
     target_type = "ip"
 
-    tags = {
-        Name     = "target-group-${var.in_ecosystem_name}-${var.in_tag_timestamp}-${count.index}"
-        Class    = var.in_ecosystem_name
-        Instance = "${var.in_ecosystem_name}-${var.in_tag_timestamp}"
-        Desc     = "This ${var.in_lb_class} load balancer backend targeting ${element(var.protocols[var.in_back_end[count.index]], 2)} traffic for ${var.in_ecosystem_name} ${var.in_tag_description}"
+    tags={
+        Name   = "target-group-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }-${ count.index }"
+        Class = "${ var.in_ecosystem_name }"
+        Instance = "${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
+        Desc   = "This ${ var.in_lb_class } load balancer backend targeting ${ element( var.protocols[ var.in_back_end[ count.index ] ], 2 ) } traffic for ${ var.in_ecosystem_name } ${ var.in_tag_description }"
     }
+
 }
+
 
 /*
  | --
@@ -165,16 +107,64 @@ resource "aws_alb_target_group" "alb_targets" {
  | -- This means that we cannot use the sensible length function on
  | -- a list of private IP addresses to determine the count.
  | --
- | --     count = length(var.in_ip_addresses)
+ | --     count = "${ length( var.in_ip_addresses ) }"
  | --
  | -- We have to break normal form with an unnecessary variable.
  | --
 */
+resource aws_lb_target_group_attachment connect {
+    count            = "${ var.in_ip_address_count }"
 
-resource "aws_lb_target_group_attachment" "connect" {
-    count            = var.in_ip_address_count
-
-    target_group_arn = element(aws_alb_target_group.alb_targets.*.arn, 0)
-    target_id        = element(var.in_ip_addresses, count.index)
-    port             = element(var.protocols[var.in_back_end[0]], 1)
+    target_group_arn = "${ element( aws_alb_target_group.alb_targets.*.arn, 0 ) }"
+    target_id        = "${ element( var.in_ip_addresses, count.index ) }"
+    port             = "${ element( var.protocols[ var.in_back_end[ 0 ] ], 1 ) }"
 }
+
+
+/*
+resource aws_alb_target_group alb_targets
+{
+    count             = "1"
+    name     = "tg-${ var.in_ecosystem_name }"
+    protocol = "HTTPS"
+    port     = "443"
+    vpc_id   = "${ var.in_vpc_id }"
+    target_type = "ip"
+
+    health_check
+    {
+        healthy_threshold   = 3
+        unhealthy_threshold = 10
+        timeout             = 5
+        interval            = 10
+        path                = "/_cluster/health"
+        port                = 443
+        protocol            = "HTTPS"
+    }
+
+    tags
+    {
+        Name   = "alb-tg-${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
+        Class = "${ var.in_ecosystem_name }"
+        Instance = "${ var.in_ecosystem_name }-${ var.in_tag_timestamp }"
+        Desc   = "This alb target group for ${ var.in_ecosystem_name } ${ var.in_tag_description }"
+    }
+
+}
+
+resource aws_alb_listener https_listener
+{
+    count             = "1"
+    load_balancer_arn = "${aws_alb.alb.arn}"
+    port                = 443
+    protocol            = "HTTPS"
+    certificate_arn     = "arn:aws:iam::<<account-id>>:server-certificate/<<certificate-id>>"
+    ssl_policy          = "ELBSecurityPolicy-2016-08"
+
+default_action {
+    target_group_arn = "${element(aws_alb_target_group.alb_targets.*.arn, 0)}"
+    type = "forward"
+  }
+}
+
+*/
--- load.balancer-variables.tf
+++ load.balancer-variables.tf
@@ -6,24 +6,21 @@
 ### ######################## ###
 ### [[variable]] in_lb_class ###
 ### ######################## ###
-variable "in_lb_class" {
-    description = "Either 'application' for a layer 7 load balancer or 'network' for a layer 4 load balancer."
-    type        = string
 
+variable in_lb_class {
+    description = "Either application for a layer 7 load balancer or network for a layer 4 load balancer."
+    default     = "application"
 }
-
 
 
 ### ######################### ###
 ### [[variable]] in_front_end ###
 ### ######################### ###
 
-variable in_front_end {
-
-
+variable in_front_end{
     description = "The front end listener configuration for this load balancer."
-    type        = list(string)
- 
+    type        = "list"
+    default     = [ "web" ]
 }
 
 
@@ -31,11 +28,10 @@ variable in_front_end {
 ### [[variable]] in_back_end ###
 ### ######################## ###
 
-variable in_back_end {
-
+variable in_back_end{
     description = "The back end target configuration for this load balancer."
-    type        = list(string)
-
+    type        = "list"
+    default     = [ "web" ]
 }
 
 
@@ -43,11 +39,9 @@ variable in_back_end {
 ### [[variable]] in_is_internal ###
 ### ########################### ###
 
-variable in_is_internal {
-
+variable in_is_internal{
     description = "If true the load balancer can be accessed externally and has a public IP address."
-    type = bool
-
+    default     = true
 }
 
 
@@ -55,31 +49,27 @@ variable in_is_internal {
 ### [[variable]] in_vpc_id ###
 ### ###################### ###
 
-variable in_vpc_id {
-    description = "vpc id"
-}
+variable in_vpc_id {}
 
 
 ### ################################## ###
 ### [[variable]] in_security_group_ids ###
 ### ################################## ###
 
-variable "in_security_group_ids" {
-
+variable "in_security_group_ids"{
     description = "ID of security group that constrains the flow of load balancer traffic."
-    type        = set(string)
-
+    type        = "list"
+    default     = []
 }
 
 
 ### ########################## ###
 ### [[variable]] in_subnet_ids ###
 ### ########################## ###
- 
-variable "in_subnet_ids" {
 
+variable "in_subnet_ids"{
     description = "IDs of subnets the network interfaces are attached to."
-    type = set(string)
+    type = "list"
 }
 
 
@@ -87,20 +77,18 @@ variable "in_subnet_ids" {
 ### [[variable]] in_ip_addresses ###
 ### ############################ ###
 
-variable in_ip_addresses {
-
+variable in_ip_addresses{
     description = "The list of IP addresses (public or private) that the load balancer will round robin spray."
-    type    = list(string)
-
+    type    = "list"
 }
+
+
 ### ################################ ###
 ### [[variable]] in_ip_address_count ###
 ### ################################ ###
 
-variable in_ip_address_count {
-
+variable in_ip_address_count{
     description = "Due to a Terraform quirk the count value must be known beforehand (at compile time so to speak)."
-    type = number
 }
 
 
@@ -108,10 +96,8 @@ variable in_ip_address_count {
 ### in_ecosystem_name ###
 ### ################# ###
 
-variable in_ecosystem_name {
-
+variable in_ecosystem_name{
     description = "Creational stamp binding all infrastructure components created on behalf of this ecosystem instance."
-    type = string
 }
 
 
@@ -119,10 +105,8 @@ variable in_ecosystem_name {
 ### in_tag_timestamp ###
 ### ################ ###
 
-variable in_tag_timestamp {
-
+variable in_tag_timestamp{
     description = "A timestamp for resource tags in the format ymmdd-hhmm like 80911-1435"
-    type = number
 }
 
 
@@ -130,9 +114,6 @@ variable in_tag_timestamp {
 ### in_tag_description ###
 ### ################## ###
 
-variable in_tag_description {
-
+variable in_tag_description{
     description = "Ubiquitous note detailing who, when, where and why for every infrastructure component."
-    type = string
 }
-
